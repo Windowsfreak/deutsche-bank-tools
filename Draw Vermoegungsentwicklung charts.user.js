150af6e19d59ab953ce538a6522cd24de6ace848
Björn Eberhardt <beberhar@thoughtworks.com>
2023-06-11 03:38:27 +0200

    change formatting

--- Draw Vermoegungsentwicklung charts.user.js
+++ Draw Vermoegungsentwicklung charts.user.js
@@ -2,7 +2,7 @@
 // @name           Deutsche Bank Vermögensentwicklung Charts
 // @name:de        Deutsche Bank Vermögensentwicklung Diagramme
 // @namespace      https://windowsfreak.de
-// @version        2.0
+// @version        2.1
 // @description    Draw Vermögensentwicklung in charts. Requires the Vermögensübersicht script.
 // @description:de Stellt die Vermögensentwicklung in Diagrammen dar. Benötigt das Vermögensübersicht-Skript.
 // @author         Björn Eberhardt
@@ -19,34 +19,34 @@
 // ==/UserScript==
 // MIT license used to import to OpenUserJS
 
-(function() {
+(function () {
     'use strict';
 
     // Navigated to Vermögensaufstellung?
     if (document.getElementsByTagName('h1')[0].innerText === 'Vermögensaufstellung' && window.location.href.indexOf('showChart=1') > -1) {
-        let chart, series;
-        const user = document.getElementById('customerNumber').childNodes[1].data.trim().replace(/\s/, '_');
+        let chart, series
+        const user = document.getElementById('customerNumber').childNodes[1].data.trim().replace(/\s/, '_')
         const users = () => {
-            const accts = [];
+            const accts = []
             for (let i = 0; i < localStorage.length; i++) {
-                const key = localStorage.key(i);
-                let match;
+                const key = localStorage.key(i)
+                let match
                 if (!!(match = key.match(/wf_(.*)_cat/))) {
-                    accts.push(match[1]);
+                    accts.push(match[1])
                 }
             }
-            return accts;
-        };
+            return accts
+        }
         const navigateTo = target => {
-            const url = new URL(window.location.href);
+            const url = new URL(window.location.href)
             for (let param of url.searchParams.keys()) {
                 if (param.indexOf('show') >= 0) {
-                    url.searchParams.delete(param);
+                    url.searchParams.delete(param)
                 }
             }
-            url.searchParams.append(target, '1');
-            window.location.href = url.href;
-        };
+            url.searchParams.append(target, '1')
+            window.location.href = url.href
+        }
         const defaultSettings = {
             marker: {
                 radius: 3
@@ -54,13 +54,14 @@
             tooltip: {
                 valueSuffix: '€'
             }
-        };
+        }
         const getNum = (bal, chg, dep = bal - chg, per = 100 * chg / dep) => ({
             bal: +bal.toFixed(2),
             chg: +chg.toFixed(2),
             dep: +dep.toFixed(2),
-            per: +per.toFixed(2)});
-        const ds = (name, overwrite, data = []) => ({ ...defaultSettings, name, data, ...overwrite });
+            per: +per.toFixed(2)
+        })
+        const ds = (name, overwrite, data = []) => ({...defaultSettings, name, data, ...overwrite})
         const prefixed_ds = name => {
             return {
                 dep: ds(name + ' Eingezahlt'),
@@ -68,29 +69,29 @@
                 chg: ds(name + ' Gewinn'),
                 per: ds(name + ' Rentabilität', {tooltip: {valueSuffix: '%'}})
             }
-        };
-        const addUp = (data, date, num) => data.reduce((acc, val) => val[0] === date && ((val[1] += num) || true) || acc, 0) || data.push([date, num]);
-        const push = (obj, date, num) => ['dep', 'bal', 'chg', 'per'].forEach(key => addUp(obj[key].data, date, num[key]));
+        }
+        const addUp = (data, date, num) => data.reduce((acc, val) => val[0] === date && ((val[1] += num) || true) || acc, 0) || data.push([date, num])
+        const push = (obj, date, num) => ['dep', 'bal', 'chg', 'per'].forEach(key => addUp(obj[key].data, date, num[key]))
 
         const load = (accts) => {
-            let wknref = {};
-            let cats = [];
-            const days = {};
+            let wknref = {}
+            let cats = []
+            const days = {}
 
             for (const acct of accts) {
                 // Collect all data from memory, collect it in the series and also initialize the HighChart and buttons
-                wknref = {...wknref, ...JSON.parse(localStorage.getItem(`wf_${acct}_wkn`) || '{}')};
-                cats = [...cats, ...JSON.parse(localStorage.getItem(`wf_${acct}_cat`) || '[]')];
+                wknref = {...wknref, ...JSON.parse(localStorage.getItem(`wf_${acct}_wkn`) || '{}')}
+                cats = [...cats, ...JSON.parse(localStorage.getItem(`wf_${acct}_cat`) || '[]')]
 
                 for (let i = 0; i < localStorage.length; i++) {
-                    const key = localStorage.key(i);
+                    const key = localStorage.key(i)
                     if (key.startsWith(`wf_${acct}_stats`)) {
-                        days[key] = JSON.parse(localStorage.getItem(key));
+                        days[key] = JSON.parse(localStorage.getItem(key))
                     }
                 }
             }
-            const rows = [];
-            Object.keys(days).sort().forEach(key => rows.push(days[key]));
+            const rows = []
+            Object.keys(days).sort().forEach(key => rows.push(days[key]))
 
             series = {
                 t: prefixed_ds('Gesamt'),
@@ -98,37 +99,37 @@
                 a: prefixed_ds('Depot'),
                 cat: {},
                 wkn: {}
-            };
+            }
 
             for (const k in cats) {
                 if (k != 'Liquidität') {
-                    series.cat[cats[k]] = prefixed_ds(cats[k]);
+                    series.cat[cats[k]] = prefixed_ds(cats[k])
                 }
             }
             for (const k in wknref) {
-                const v = wknref[k];
-                series.wkn[k] = prefixed_ds(k + ' - ' + v);
+                const v = wknref[k]
+                series.wkn[k] = prefixed_ds(k + ' - ' + v)
             }
 
             rows.forEach(row => {
-                const segments = /(\d{4})(\d{2})(\d{2})/.exec(row.d_str);
-                const date = Date.parse(segments[1] + '-' + segments[2] + '-' + segments[3]);
-                const t = getNum(row.tbal, row.tchg);
-                const b = getNum(row.bbal, row.bchg);
-                const a = getNum(row.tbal - row.bbal, row.tchg - row.bchg);
-                push(series.t, date, t);
-                push(series.b, date, b);
-                push(series.a, date, a);
+                const segments = /(\d{4})(\d{2})(\d{2})/.exec(row.d_str)
+                const date = Date.parse(segments[1] + '-' + segments[2] + '-' + segments[3])
+                const t = getNum(row.tbal, row.tchg)
+                const b = getNum(row.bbal, row.bchg)
+                const a = getNum(row.tbal - row.bbal, row.tchg - row.bchg)
+                push(series.t, date, t)
+                push(series.b, date, b)
+                push(series.a, date, a)
                 for (let key in row.wkns) {
-                    push(series.wkn[key], date, getNum(row.wkns[key].bal, row.wkns[key].chg));
+                    push(series.wkn[key], date, getNum(row.wkns[key].bal, row.wkns[key].chg))
                 }
                 for (let key in row.cats) {
                     if (key != 'Liquidität') {
-                        push(series.cat[key], date, getNum(row.cats[key].bal, row.cats[key].chg));
+                        push(series.cat[key], date, getNum(row.cats[key].bal, row.cats[key].chg))
                     }
                 }
-            });
-        };
+            })
+        }
 
         const init = () => {
             Highcharts.setOptions({
@@ -151,9 +152,9 @@
                     resetZoom: "Zoom zurücksetzen",
                     resetZoomTitle: "Zoom zurücksetzen"
                 }
-            });
+            })
 
-            const div = document.createElement('div');
+            const div = document.createElement('div')
             div.innerHTML = `
                 Der Abruf der Daten wurde erfolgreich beendet.<br />
                 <input type="button" value="Tabelle zeigen" class="button nextStep">
@@ -172,24 +173,24 @@
                 <input type="button" value="Beides">
                 <input type="button" value="Kategorien">
                 <input type="button" value="Fonds">
-                <div id="container" style="height: 800px"></div>`;
-            [
-                () => attrs(['dep']),
-                () => attrs(['bal']),
-                () => attrs(['dep', 'bal']),
-                () => attrs(['chg']),
-                () => attrs(['per']),
-                () => types(['t']),
-                () => types(['b']),
-                () => types(['a']),
-                () => types(['t', 'b', 'a']),
-                () => types(['cat']),
-                () => types(['wkn'])
-            ].forEach((v, k) => div.getElementsByTagName('input')[k + 3].onclick = v);
-            div.getElementsByTagName('input')[0].onclick = () => navigateTo('showTable');
-            div.getElementsByTagName('input')[1].onclick = () => navigateTo('showChart');
-            div.getElementsByTagName('input')[2].onclick = () => load(prompt('Konten mit Komma getrennt angeben, zur Auswahl stehen: ' + users().join(','), user).split(','));
-            document.getElementById('assetsOverviewForm').prepend(div);
+                <div id="container" style="height: 800px"></div>`
+                [
+                    () => attrs(['dep']),
+                    () => attrs(['bal']),
+                    () => attrs(['dep', 'bal']),
+                    () => attrs(['chg']),
+                    () => attrs(['per']),
+                    () => types(['t']),
+                    () => types(['b']),
+                    () => types(['a']),
+                    () => types(['t', 'b', 'a']),
+                    () => types(['cat']),
+                    () => types(['wkn'])
+                ].forEach((v, k) => div.getElementsByTagName('input')[k + 3].onclick = v)
+            div.getElementsByTagName('input')[0].onclick = () => navigateTo('showTable')
+            div.getElementsByTagName('input')[1].onclick = () => navigateTo('showChart')
+            div.getElementsByTagName('input')[2].onclick = () => load(prompt('Konten mit Komma getrennt angeben, zur Auswahl stehen: ' + users().join(','), user).split(','))
+            document.getElementById('assetsOverviewForm').prepend(div)
             chart = Highcharts.stockChart('container', {
                 boost: {
                     useGPUTranslations: true
@@ -209,23 +210,23 @@
                 legend: {
                     enabled: true
                 }
-            });
-            redraw();
-        };
+            })
+            redraw()
+        }
 
-        let chosen_attributes = ['dep', 'bal'];
-        let chosen_types = ['a'];
+        let chosen_attributes = ['dep', 'bal']
+        let chosen_types = ['a']
         const redraw = _ => {
             while (chart.series.length > 0) {
-                chart.series[0].remove(false);
+                chart.series[0].remove(false)
             }
-            const chosen = [];
+            const chosen = []
             chosen.push(...['t', 'b', 'a'].filter(e => chosen_types.includes(e)).map(e => series[e])
                 .flatMap(
-                    x => chosen_attributes.map(e => x[e])));
+                    x => chosen_attributes.map(e => x[e])))
             chosen.push(...['cat', 'wkn'].filter(e => chosen_types.includes(e)).map(e => series[e])
                 .flatMap(y => Object.values(y).flatMap(
-                    x => chosen_attributes.map(e => x[e]))));
+                    x => chosen_attributes.map(e => x[e]))))
             chosen.forEach(
                 x => chart.addSeries(x, false)
             );
@@ -235,21 +236,21 @@
                         labels: {
                             format: '{value}%'
                         }
-                    }, false);
+                    }, false)
                 } else {
                     chart.yAxis[0].update({
                         labels: {
                             format: null
                         }
-                    }, false);
+                    }, false)
                 }
-                chart.redraw();
+                chart.redraw()
             }
-        };
-        const attrs = choice => redraw(chosen_attributes = choice) && false;
-        const types = choice => redraw(chosen_types = choice) && false;
+        }
+        const attrs = choice => redraw(chosen_attributes = choice) && false
+        const types = choice => redraw(chosen_types = choice) && false
 
-        load([user]);
-        init();
+        load([user])
+        init()
     }
-})();
+})()
